d868bbe9f371d9f1c6b694f19cb3360cbbb81280
Атабек Алыбаев <atabekalybaev@MacBook-Pro-Atabek.local>
2023-05-18 20:18:13 +0600

    add ip addres to allowed hosts

--- ananas/settings.py
+++ ananas/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '13.53.139.126']
 
 
 # Application definition
